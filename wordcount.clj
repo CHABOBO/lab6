b9099bcab12eeed574d05a024f83a02369387c80
Elizabeth Chabot <elizchabot@gmail.com>
2017-07-17 23:30:03 -0500

    wordcount

---
+++ wordcount.clj
@@ -0,0 +1,31 @@
+(ns sentences
+  (:use     [streamparse.specs])
+  (:gen-class))
+
+(defn sentences [options]
+   [
+    ;; spout configuration
+    {"sentences-spout" (python-spout-spec
+          options
+          "spouts.sentences.Sentences"
+          ["sentence"]
+          )
+    }
+    ;; bolt configuration 1
+    {"ParseTweet-bolt" (python-bolt-spec
+          options
+          {"sentences-spout" :shuffle}
+          "bolts.parse.ParseTweet"
+          ["validwords"]
+          )
+    }
+    ;; bolt configuration 2
+    {"TweetCounter-bolt" (python-bolt-spec
+          options
+          {"sentences-spout" :shuffle}
+          "bolts.tweetcounter.TweetCounter"
+          ["word" "count"]
+          :p 3
+          )
+    }
+  ]
